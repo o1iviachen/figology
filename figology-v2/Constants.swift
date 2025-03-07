9482171f6434a833f04aab7e792ec5836e6f7689
emily <ezhan322@gmail.com>
2025-03-07 12:44:37 -0500

    constants

---
+++ figology-v2/Constants.swift
@@ -0,0 +1,36 @@
+//
+//  Constants.swift
+//  figology-v2
+//
+//  Created by emily zhang on 2025-03-07.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    static let logInSegue = "WelcomeToLogIn"
+    static let signUpSegue = "WelcomeToSignUp"
+    static let mainLogInSegue = "LogInToMain"
+    static let mainSignUpSegue = "SignUpToMain"
+    static let systemCellIdentifier = "SystemCell"
+    static let foodCellIdentifier = "FoodCell"
+    static let cellNibName = "SystemCell"
+    static let cellFoodName = "FoodCell"
+    static let logOutCellIdentifier = "LogOutCell"
+    static let logOutCellNib = "LogOutCell"
+    static let searchSegue = "searchSegue"
+    static let signUpCalculatorSegue = "signUpToCalculator"
+    static let calculatorMainSegue = "calculatorToMain"
+    static let calculatorProSegue = "calculatorToPro"
+    static let proMainSegue = "proToMain"
+    static let logInCalculatorSegue = "logInToCalculator"
+    static let profileSelectorSegue = "profileToSelector"
+    static let profileCalculatorSegue = "profileToCalculator"
+    static let profilePasswordSegue = "profileToPassword"
+    static let profileSupportSegue = "profileToSupport"
+    static let profileAboutSegue = "profileToAbout"
+    static let logInPasswordSegue = "logInToPassword"
+    static let searchResultSegue = "searchToResult"
+    static let foodEditSegue = "foodToEdit"
+}
---
+++ figology-v2/Controller/LogInViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LogInViewController.swift
+//  figology-v2
+//
+//  Created by emily zhang on 2025-03-07.
+//
+
+import Foundation
