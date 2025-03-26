6fa470ad8537981305e5604bdda7589078451d80
emily <ezhan322@gmail.com>
2025-03-26 11:47:08 -0400

    calendar

--- figology-v2/Controller/CalendarViewController.swift
+++ figology-v2/Controller/CalendarViewController.swift
@@ -2,7 +2,40 @@
 //  CalendarViewController.swift
 //  figology-v2
 //
-//  Created by emily zhang on 2025-03-26.
+//  Created by emily zhang on 2025-03-24.
 //
 
-import Foundation
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createCalendar()
+    }
+    func createCalendar() {
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 600),
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        
+        ])
+    }
+}
+
+extension CalendarViewController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
+}
